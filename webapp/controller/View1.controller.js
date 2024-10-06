96e95959046303098dbf2d847736e811953a6905
Данило Карчевський <karchevsky0danil@gmail.com>
2024-10-06 20:36:15 +0000

    Initial Commit @10/6/2024, 8:36:15 PM

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+],
+function (Controller, MessageBox) {
+    "use strict";
+
+    return Controller.extend("sap.btp.helloworld.controller.View1", {
+        onInit: function () {
+        },
+        onPress: function () {
+            MessageBox.alert("You have been alerted!");
+        }
+    });
+});
